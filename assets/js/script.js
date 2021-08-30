83733f7fc9471cb64e8a961580b6fe7dbf0baf3c
ggamb <glenngambardella372@gmail.com>
2021-08-30 11:11:14 -0400

    added edits and blurs

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,37 +45,45 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-$(".list-group").on("click", "p", function(){
-  var text = $(this).text();
-  var textInput = $("<textarea>")
-    .addClass("form-control")
-    .val(text);
+// task text was clicked
+$(".list-group").on("click", "p", function() {
+  // get current text of p element
+  var text = $(this)
+    .text()
+    .trim();
 
+  // replace p element with a new textarea
+  var textInput = $("<textarea>").addClass("form-control").val(text);
   $(this).replaceWith(textInput);
+
+  // auto focus new element
   textInput.trigger("focus");
 });
 
-$("list-group").on("blur", "textarea", function(){
-  var text = $(this)
-    .val()
-    .trim();
+// editable field was un-focused
+$(".list-group").on("blur", "textarea", function() {
+  // get current value of textarea
+  var text = $(this).val();
 
+  // get status type and position in the list
   var status = $(this)
     .closest(".list-group")
     .attr("id")
     .replace("list-", "");
-
   var index = $(this)
     .closest(".list-group-item")
     .index();
 
+  // update task in array and re-save to localstorage
   tasks[status][index].text = text;
   saveTasks();
 
+  // recreate p element
   var taskP = $("<p>")
     .addClass("m-1")
     .text(text);
 
+  // replace textarea with new content
   $(this).replaceWith(taskP);
 });
 
